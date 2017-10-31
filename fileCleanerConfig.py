cb19f2f180e78bb613dd0b3860fa71a7db589ccf
payzaburou <pampkin728@yahoo.co.jp>
2017-10-31 07:27:07 -0700

    fileCleanerの設定ファイルを追加。

---
+++ fileCleanerConfig.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#=================================
+# file cleaner config settings. 
+#=================================
+
+# 対象ディレクトリ
+TARGET_DIR = ''
+# 保存ディレクトリ
+SAVE_DIR = ''
+# 無視ファイル
+IGNORE_FILE = 'ignore.txt'
